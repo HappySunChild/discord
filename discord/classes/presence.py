2275385283615f583601e9297b4c7618155517ea
name <nothappysunchild@gmail.com>
2024-08-06 18:47:55 -0400

    added ClientStatus class

--- discord/classes/presence.py
+++ discord/classes/presence.py
@@ -3,10 +3,32 @@ from ..enums import PresenceStatusType
 
 from .activity import Activity, ActivityType
 
+class ClientStatus:
+	def __init__(self, status_data: dict) -> None:
+		self.desktop_presence = PresenceStatusType(status_data.get('desktop', 'offline'))
+		self.web_presence = PresenceStatusType(status_data.get('web', 'offline'))
+		self.mobile_presence = PresenceStatusType(status_data.get('mobile', 'offline'))
+		
+		self.platforms = list(status_data.keys())
+		
+		self._raw = status_data
+	
+	@property
+	def is_web(self):
+		return self.web_presence != PresenceStatusType.Offline
+	
+	@property
+	def is_desktop(self):
+		return self.desktop_presence != PresenceStatusType.Offline
+	
+	@property
+	def is_mobile(self):
+		return self.mobile_presence != PresenceStatusType.Offline
+
 class Presence:
 	def __init__(self, presence_data: dict) -> None:
 		self.status = PresenceStatusType(presence_data['status'])
-		self.client_status = presence_data['client_status']
+		self.client_status = ClientStatus(presence_data['client_status'])
 		
 		self.activities = [Activity(data) for data in presence_data['activities']]
 		
