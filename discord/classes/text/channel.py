21bf67e7fad74d1fb877a80baddee3986f72dce8
name <nothappysunchild@gmail.com>
2024-08-06 18:48:14 -0400

    added Channel.get_messages()

--- discord/classes/text/channel.py
+++ discord/classes/text/channel.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from .message import Message
+
 if TYPE_CHECKING:
 	from ..client import DiscordClient
 
@@ -9,14 +11,26 @@ class Channel:
 		self.client = client
 		self.id = id
 	
+	def get_messages(self, limit: int = 50, before: int = None):
+		messages_data, _ =self.client.requester.get(
+			url=f'https://discord.com/api/v9/channels/{self.id}/messages',
+			params={
+				'limit': limit,
+				'before': before
+			}
+		)
+		
+		return [
+			Message(message_data=message_data)
+			for message_data in messages_data
+		]
+	
 	def send_message(self, message: str):
 		payload = {
 			'content': message,
 		}
 		
-		_, res = self.client.requester.post(
+		self.client.requester.post(
 			url=f'https://discord.com/api/v9/channels/{self.id}/messages',
 			json=payload
 		)
-		
-		print(res, res.json())
