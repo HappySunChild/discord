0cec08322c5f841c8f5a459cac465e3f2e29eb5b
HappySunChild <nothappysunchild@gmail.com>
2025-03-09 20:09:02 -0400

    Update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
 	name='discord',
 	description='',
-	version='1.0.0',
+	version='1.0.2',
 	author='HappySunChild',
 	url='https://github.com/HappySunChild/socket2',
 	
